fb152f8c77a97fe09770ef900b7507c5f00cfc05
chenzhihao <cabtravis@gmail.com>
2016-11-04 12:03:24 -0700

    when data update, do render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import {reactify} from './observer';
 
-let data = {
-    user: {
-        name: "name",
-        age: "24"
-    },
-    address: {
-        city: "beijing"
-    }
-};
+class Vue {
+  constructor (options) {
+    this.$options = options;
+    this._data = options.data;
+    reactify(options.data, this._update.bind(this));
+  }
 
-window.data = data;
+  _update () {
+    this.$options.render()
+  }
+}
 
-reactify(data);
+var demo = new Vue({
+  el: '#demo',
+  data: {
+  user: {
+    name: "name",
+    age: "24"
+  },
+  address: {
+    city: "beijing"
+  }
+},
+  render(){
+    console.log("我要render了")
+  }
+});
+
+window.demo = demo;
+
+setTimeout(function () {
+  demo._data.user.name = 'newName';
+}, 3000);
--- src/observer.js
+++ src/observer.js
@@ -4,45 +4,46 @@
  * @param data
  * @constructor
  */
-function Observer (data) {
+function Observer (data, cb) {
   this.value = data;
-  this.walk(data);
+  this.walk(data, cb);
 }
 
-Observer.prototype.defineReactive = function (obj, key, val) {
+Observer.prototype.defineReactive = function (obj, key, val, cb) {
   Object.defineProperty(obj, key, {
     enumerable: true,
     configurable: true,
     get: function () {
       console.log('Access: ' + key);
-      return val
+      return val;
     },
     set: function (newVal) {
-      console.log('Config on: ' + key);
-      console.log('New: ' + key + ' = ' + newVal);
+      console.log('Config on: ' + key + ', old value: ' + val);
+      console.log('New: ' + key + ' : ' + newVal);
       if (newVal === val) return;
+      cb(val, newVal);
       val = newVal;
-      reactify(val);
+      reactify(val, cb);
     }
   });
 };
 
-Observer.prototype.walk = function (obj) {
+Observer.prototype.walk = function (obj, cb) {
   for (let key in obj) {
     if (!obj.hasOwnProperty(key)) return;
 
     if (typeof obj[key] === 'object') {
-      reactify(obj[key]);
+      reactify(obj[key], cb);
     }
 
-    this.defineReactive(obj, key, obj[key]);
+    this.defineReactive(obj, key, obj[key], cb);
   }
 };
 
-export function reactify (value) {
+export function reactify (value, cb) {
   if (!value || typeof value !== 'object') {
     return
   }
-  return new Observer(value);
+  return new Observer(value, cb);
 }
 
